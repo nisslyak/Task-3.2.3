d8bf565bf4c57f16d35b1aa84fee049f1c7df1b1
nisslyak <nissopodolyak@gmail.com>
2024-07-20 16:06:42 +1000

    Add project files.

---
+++ Task 3.2.3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_3._2._3
+{
+   class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string myName;
+            myName = "Nisso";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
